80d144aa98b997052f673bea5e2d7f492e2176a8
Laia Muniesa <laia.muniesa@babelgroup.com>
2024-03-14 11:55:45 +0100

    plantilla

--- src/main/java/es/babel/McRonalds/model/Pedido.java
+++ src/main/java/es/babel/McRonalds/model/Pedido.java
@@ -2,6 +2,9 @@ package es.babel.McRonalds.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Pedido {
+    private List<Producto> listProductos;
 }
--- src/main/java/es/babel/McRonalds/model/Plantilla.java
+++ src/main/java/es/babel/McRonalds/model/Plantilla.java
@@ -7,5 +7,5 @@ import java.util.List;
 @Data
 public class Plantilla {
 
-    private List<Trabajador> listaTrabajadores;
+    private List<Trabajador> listTrabajadores;
 }
