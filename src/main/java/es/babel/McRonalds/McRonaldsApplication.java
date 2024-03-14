f63b3affb15f45168d52c5fa7666cd615c2eb777
Miguel Mier <miguel.mier@babelgroup.com>
2024-03-14 09:59:41 +0100

    commit inicial

---
+++ src/main/java/es/babel/McRonalds/McRonaldsApplication.java
@@ -0,0 +1,13 @@
+package es.babel.McRonalds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McRonaldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McRonaldsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/babel/McRonalds/McRonaldsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.babel.McRonalds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McRonaldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
